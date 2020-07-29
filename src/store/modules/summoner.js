110509c7c242785d1579099e32307ad93462feef
sgdg <seungdeng17@gmail.com>
2020-07-29 23:53:26 +0900

    [#5] feat: 소환사 정보를 받아오는 리덕스 모듈 작성
    - api url 추가

---
+++ src/store/modules/summoner.js
@@ -0,0 +1,41 @@
+import { API } from '@constants/url';
+import { checkResponseData } from '@utils/util';
+
+const GET_DATA_SUCCESS = 'summoner/GET_DATA_SUCCESS';
+const GET_DATA_ERROR = 'summoner/GET_DATA_ERROR';
+
+export const getSummoner = summonerName => async dispatch => {
+    try {
+        const response = await fetch(API.GET_SUMMONER(summonerName));
+        if (!checkResponseData(response)) throw (new Error(response.status));
+        const data = await response.json();
+        dispatch({ type: GET_DATA_SUCCESS, payload: data });
+    } catch (error) {
+        dispatch({ type: GET_DATA_ERROR, payload: error });
+    }
+};
+
+const initialState = {
+    summonerData: null,
+    error: null,
+};
+
+const summoner = (state = initialState, action) => {
+    switch (action.type) {
+        case GET_DATA_SUCCESS:
+            return {
+                ...state,
+                summonerData: action.payload.summoner,
+            }
+        case GET_DATA_ERROR: {
+            return {
+                ...state,
+                error: action.payload,
+            }
+        }
+        default:
+            return state;
+    }
+};
+
+export default summoner;
