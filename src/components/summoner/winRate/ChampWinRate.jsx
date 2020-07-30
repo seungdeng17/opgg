d26adc0d60de79c5d0c17b90b64e4ea64344a4d0
sgdg <seungdeng17@gmail.com>
2020-07-30 22:26:38 +0900

    [#10] feat: 챔피언 승률 출력 기능 추가

--- src/components/summoner/winRate/ChampWinRate.jsx
+++ src/components/summoner/winRate/ChampWinRate.jsx
@@ -1,7 +1,94 @@
 import React from "react";
+import styled from "styled-components";
+import { rateCalculator, kdaColorPicker } from "@utils/util";
+
+const ChampWinRateItem = styled.li`
+  padding: 4px 15px;
+  border-bottom: 1px solid #cdd2d2;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  font-size: 11px;
+  color: #879292;
+
+  :last-child {
+    border-bottom: none;
+  }
+`;
+
+const ChampImg = styled.img`
+  width: 45px;
+  height: 45px;
+  border-radius: 50%;
+  margin-right: 10px;
+`;
+
+const InfoText = styled.div`
+  width: ${({ width }) => `${width}px`};
+  height: 32px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  text-align: ${({ align }) => align};
+
+  .top {
+    color: ${({ kdaColor }) => (kdaColor ? kdaColor : "#5e5e5e")};
+    font-size: 13px;
+    font-weight: 600;
+    cursor: default;
+  }
+
+  div {
+    overflow: hidden;
+    white-space: nowrap;
+    text-overflow: ellipsis;
+  }
+
+  :last-child {
+    margin-right: 0;
+  }
+`;
 
 const ChampWinRate = ({ champions }) => {
-  return <div>ChampWinRate</div>;
+  const championsSort = champions.sort((a, b) => b.games - a.games);
+
+  const champList = [];
+  const champWinRateList = championsSort.map((data) => {
+    const { cs, games, key, name, imageUrl } = data;
+    if (champList.includes(key)) return null;
+    champList.push(key);
+
+    const { csRate, killRate, assistRate, deathRate, kdaScore, winRate } = rateCalculator(data);
+    const kdaColor = kdaColorPicker(+kdaScore);
+
+    console.log(kdaColor);
+
+    return (
+      <ChampWinRateItem key={key}>
+        <ChampImg src={imageUrl} alt="champ-img" />
+        <InfoText width={68} align={"left"}>
+          <div className="top" title={name}>
+            {name}
+          </div>
+          <div>
+            CS {cs} ({csRate})
+          </div>
+        </InfoText>
+        <InfoText width={88} align={"center"} kdaColor={kdaColor}>
+          <div className="top">{kdaScore}:1 평점</div>
+          <div>
+            {killRate} / {deathRate} / {assistRate}
+          </div>
+        </InfoText>
+        <InfoText width={40} align={"center"}>
+          <div className="top">{winRate}%</div>
+          <div>{games}게임</div>
+        </InfoText>
+      </ChampWinRateItem>
+    );
+  });
+
+  return <ul>{champWinRateList}</ul>;
 };
 
 export default ChampWinRate;
--- src/utils/util.js
+++ src/utils/util.js
@@ -13,3 +13,25 @@ export const tierConverter = (tier, division) => {
     }
     return `${tier} ${divisions[division]}`;
 };
+
+export const rateCalculator = (data) => {
+    const { cs, kills, assists, deaths, wins, games } = data;
+
+    const csRate = (cs / games).toFixed(1);
+    const killRate = (kills / games).toFixed(1);
+    const assistRate = (assists / games).toFixed(1);
+    const deathRate = (deaths / games).toFixed(1);
+    const kdaScore = ((kills + assists) / deaths).toFixed(2);
+    const winRate = Math.round((wins / games) * 100);
+
+    return {
+        csRate, killRate, assistRate, deathRate, kdaScore, winRate
+    }
+}
+
+export const kdaColorPicker = (kda) => {
+    if (kda >= 5) return '#e19205';
+    if (kda >= 4) return '#1f8ecd';
+    if (kda >= 3) return '#2daf7f';
+    return null;
+}
