c02b81765e08e54aa7db1b7eb0936788773cf64e
sgdg <seungdeng17@gmail.com>
2020-07-30 14:09:33 +0900

    feat: NoResult 컴포넌트 분리

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,45 +2,19 @@ import React from "react";
 import styled from "styled-components";
 import { useSelector } from "react-redux";
 
+import NoResult from "@components/etc/NoResult";
 import Header from "@components/header/Header";
 import Profile from "@components/summoner/profile/Profile";
-
-import dinger from "@assets/images/dinger.png";
 
 const MainWrap = styled.div`
   width: 1440px;
   margin: 0 auto;
 `;
 
-const NoResult = styled.div`
-  height: calc(100vh - 97px);
-  background-color: #eaeaea;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
-
-  p {
-    font-family: "AppleSDGothicNeo-Regular";
-    margin-top: 20px;
-    font-size: 35px;
-    font-weight: 600;
-    color: #879292;
-  }
-`;
-
 const Main = () => {
   const { summonerData, error } = useSelector(({ summoner }) => summoner);
 
-  const mainContent =
-    summonerData && !error ? (
-      <Profile {...{ summonerData }} />
-    ) : (
-      <NoResult>
-        <img src={dinger} alt="dinger-img" />
-        <p>소환사명을 입력해주세요.</p>
-      </NoResult>
-    );
+  const mainContent = summonerData && !error ? <Profile {...{ summonerData }} /> : <NoResult />;
 
   return (
     <MainWrap>
