127027348f3f40e8cae5c4d4b524d218ab1c1ed8
sgdg <seungdeng17@gmail.com>
2020-07-30 16:12:44 +0900

    feat: tierConverter 유틸 함수 추가

--- src/utils/util.js
+++ src/utils/util.js
@@ -1,3 +1,15 @@
 export const checkResponseData = response => response.ok && (response.status >= 200 && response.status <= 207);
 
 export const numberComma = (number) => number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+
+export const tierConverter = (tier, division) => {
+    if (tier === 'Challenger' || tier === 'Grandmaster' || tier === 'Master') return tier;
+
+    const divisions = {
+        'i': 1,
+        'ii': 2,
+        'iii': 3,
+        'iv': 4
+    }
+    return `${tier} ${divisions[division]}`;
+};
