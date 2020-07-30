e8a66b742aeb1a4313e74399b776d77fbf0db6d7
sgdg <seungdeng17@gmail.com>
2020-07-30 23:45:53 +0900

    refactor: onClick 이벤트 콜백 함수 분리

--- src/components/common/Tabs.jsx
+++ src/components/common/Tabs.jsx
@@ -3,8 +3,9 @@ import React, { useState } from "react";
 const Tabs = ({ children, TabButtonWrap, TabButton }) => {
   const [selected, setSelected] = useState(0);
   const tabs = children.map((c, i) => {
+    const handleClick = () => setSelected(i);
     return (
-      <TabButton key={i} selected={i === selected} onClick={() => setSelected(i)}>
+      <TabButton key={i} selected={i === selected} onClick={handleClick}>
         {c.props.title}
       </TabButton>
     );
