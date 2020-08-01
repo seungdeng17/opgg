cbf863301d536acfaf622943f8ed2fbe1969db5f
sgdg <seungdeng17@gmail.com>
2020-08-01 23:11:18 +0900

    [#16] feat: 화살표 아이콘 추가

--- src/components/summoner/match/matchList/MoreInfoButton.jsx
+++ src/components/summoner/match/matchList/MoreInfoButton.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import { GoChevronDown } from "react-icons/go";
 
 const MoreInfoButtonWrap = styled.div`
   position: absolute;
@@ -12,19 +13,43 @@ const MoreInfoButtonWrap = styled.div`
   &.win-game {
     background-color: #7fb0e1;
     border-color: #549dc7;
+    color: #04609e;
   }
   &.lose-game {
     background-color: #e89c95;
     border-color: #c8817c;
+    color: #c1433e;
   }
   &.re-game {
     background-color: #a7a7a7;
     border-color: #999;
+    color: #555;
+  }
+`;
+
+const ArrowWrap = styled.div`
+  position: relative;
+  width: 100%;
+  height: 100%;
+  .arrow {
+    position: absolute;
+    z-index: 1;
+    left: 50%;
+    bottom: 4px;
+    font-size: 23px;
+    transform: translateX(-50%);
+    cursor: pointer;
   }
 `;
 
 const MoreInfoButton = ({ resultClassName }) => {
-  return <MoreInfoButtonWrap className={resultClassName}></MoreInfoButtonWrap>;
+  return (
+    <MoreInfoButtonWrap className={resultClassName}>
+      <ArrowWrap>
+        <GoChevronDown className="arrow" />
+      </ArrowWrap>
+    </MoreInfoButtonWrap>
+  );
 };
 
 export default MoreInfoButton;
