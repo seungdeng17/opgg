5a1d4560835d72adb5c5ceaeac46a984926e895f
sgdg <seungdeng17@gmail.com>
2020-08-01 14:30:55 +0900

    [#16] feat: 게임 결과 className과 text 반환하는 유틸 추가

--- src/utils/util.js
+++ src/utils/util.js
@@ -4,7 +4,6 @@ export const getNumberComma = (number) => number.toString().replace(/\B(?=(\d{3}
 
 export const getTierName = (tier, division) => {
     if (tier === 'Challenger' || tier === 'Grandmaster' || tier === 'Master') return tier;
-
     const divisions = {
         'i': 1,
         'ii': 2,
@@ -16,30 +15,26 @@ export const getTierName = (tier, division) => {
 
 export const calculateRate = (data) => {
     const { cs, kills, assists, deaths, wins, games } = data;
-
     const csRate = (cs / games).toFixed(1);
     const killRate = (kills / games).toFixed(1);
     const assistRate = (assists / games).toFixed(1);
     const deathRate = (deaths / games).toFixed(1);
     const kdaScore = ((kills + assists) / deaths).toFixed(2);
     const winRate = Math.round((wins / games) * 100);
-
-    return {
-        csRate, killRate, assistRate, deathRate, kdaScore, winRate
-    }
-}
+    return { csRate, killRate, assistRate, deathRate, kdaScore, winRate };
+};
 
 export const getKdaScoreColor = (kda) => {
     if (kda >= 5) return '#e19205';
     if (kda >= 4) return '#1f8ecd';
     if (kda >= 3) return '#2daf7f';
     return null;
-}
+};
 
 export const getWinRateColor = (winRate) => {
     if (winRate >= 60) return '#c6443e';
     return null;
-}
+};
 
 export const translatePositionName = (position) => {
     switch (position) {
@@ -56,4 +51,18 @@ export const translatePositionName = (position) => {
         default:
             break;
     }
-}
+};
+
+export const getGameResult = (gameLength, isWin) => {
+    const resultClassName = gameLength <= 300 ? "re-game" : isWin ? "win-game" : "loss-game";
+    switch (resultClassName) {
+        case 'win-game':
+            return { resultClassName, resultText: '승리' };
+        case 'loss-game':
+            return { resultClassName, resultText: '패배' };
+        case 're-game':
+            return { resultClassName, resultText: '다시하기' };
+        default:
+            break;
+    }
+};
